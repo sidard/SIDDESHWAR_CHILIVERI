d0a8958fb1737d63e7700a81fa926639736e2d42
SIDARDUINO <108972746+SIDARDUINO@users.noreply.github.com>
2022-07-11 10:55:26 +0530

    Added day4_q8.c
    Output:
    Enter the data: 63
    Equivalent binary is: 
    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 
    Enter the bit number to set: 8
    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1 1 1 1 1

---
+++ DAY_4/day4_q8.c
@@ -0,0 +1,34 @@
+/*Author: Siddeshwar Chiliveri
+Programming Language: C
+IDE used: Eclipse
+Compiler Used : GCC C Compiler*/
+/*3)HOW TO COMPLIMENT A BIT */
+
+
+#include <stdio.h>
+
+int main()
+{
+   int i,data,bit;
+   printf("Enter the data: ");
+   scanf("%d",&data);
+   puts("Equivalent binary is: ");
+   for(i=31;i>=0;i--)
+  {
+	printf("%d ",((data>>i)&1));
+  }
+   printf("\n");
+   printf("Enter the bit number to set: ");   
+   scanf("%d",&bit);
+   
+  
+  printf("\n");
+ 
+  data = data ^ (1<<bit); // COMPLIMENT A BIT
+    for(i=31;i>=0;i--)
+  {
+	printf("%d ",((data>>i)&1));
+  }
+     
+   
+}
