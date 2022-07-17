d8e54618ef7403bc64bc96f305293008093f6571
SIDARDUINO <108972746+SIDARDUINO@users.noreply.github.com>
2022-07-17 20:00:37 +0530

    Final update in day13_q1.c
    20 Sample Data:
    
    0 msec: 0x0 | 10 msec: 0x0 | 20 msec: 0x0 | 30 msec: 0x0 | 40 msec: 0x0 | 50 msec: 0xef | 60 msec: 0x6 | 70 msec: 0x84 | 80 msec: 0xf8 | 90 msec: 0x39 | 100 msec: 0x39 | 110 msec: 0x39 | 120 msec: 0xf6 | 130 msec: 0x57 | 140 msec: 0x43 | 150 msec: 0x42 | 160 msec: 0x69 | 170 msec: 0x75 | 180 msec: 0x16 | 190 msec: 0x19 | 
    ---------------------------------------------------------------------------------------
    
    Time stamp where data is coming as noise:
    
    0 msec , 10 msec , 20 msec , 30 msec , 40 msec , 80 msec , 120 msec , 
    ---------------------------------------------------------------------------------------
    
    No-Noise Data:
    50 msec: 0xef | 60 msec: 0x6 | 70 msec: 0x84 | 90 msec: 0x39 | 100 msec: 0x39 | 110 msec: 0x39 | 130 msec: 0x57 | 140 msec: 0x43 | 150 msec: 0x42 | 160 msec: 0x69 | 170 msec: 0x75 | 180 msec: 0x16 | 190 msec: 0x19 |

--- DAY_13/day13_q1.c
+++ DAY_13/day13_q1.c
@@ -15,60 +15,76 @@ Output 2 : Delete the noise data and write valid sensor values to new 2-dimensio
 print them – timestamp and values.
 
 Algorithm:
-1. Read elements from Sensor_values array one by one.
-2. Check the Sensor_data is less than 0x05 and greater than 0xF0, if found true then print time stamp, else move to next element.
-3. If above step gives false then create an Sensor_noNoise_Vlaue Array and add the no noise elements in it.
-4. Print the Sensor_noNoise_Vlaue Array 
+1.Initlialize a count of Samples to 0
+2. Read Sensor data samples captured at every 10 msec from acceleration uptil count of samples reached to 20.
+3. If Sensor data read is smaller than 0x05 & greater than 0xF0, print the time stamp of current sensor data read.
+4. If Sensor data read does not meet above condition then store the time stamp & current sensor data in "Sensor_noNoiceValue" Array
 *******************************************************************************/
 #include<stdio.h>
 #include <stdlib.h>
+
+#define Noise_lowRange  0x05 // Set Noise Range1 : less than 0x05
+#define Noise_highRange  0xF0 // Set Noise Range2 : greater than 0xF0
+
+
+
+/*Function to Display the Array elements*/
+void Disp(int (*arr)[2], int size)
+{
+	for(int i = 0;i<size;i++)
+	{
+		printf("%d msec: 0x%x | ",arr[i][0], arr[i][1]);fflush(stdout);
+	}
+}
+/*Function to Check the sensor data is noise or not*/
+int CheckNoise(int data)
+{
+	if((data<Noise_lowRange) || (data>Noise_highRange))
+		return 1;
+	else
+		return 0;
+}
+int CheckMalfun(int data1, int data2)
+{
+	if(data1 == data2)
+		return 1;
+	else
+		return 0;
+}
 int main()
 {
-    short int Sensor_Values[20][2] = {{0,0x00},{10,0x00},{20,0x0F},{30, 0xFF}, {40, 0xFF},
-    						{50,0x05}, {60, 0xAA}, {70, 0x55}, {80, 0xFE},
-                        {90, 0x00}, {100, 0x00}, {110, 0x00}, {120, 0xFF}, {130, 0xFF}, {140, 0xFF}, {150, 0xFF},
-                        {160,0x11}, {170, 0x33}, {180, 0x99}, {190, 0xE9}};
+	/*Acceleration Sensor Data*/
+	int Sensor_Values[20][2] = {{0,0x00},{10,0x00},{20,0x00},{30,0x00},{40,0x00},{50,0xEF},
+								{60,0x06},{70,0x84},{80,0xF8},{90,0x39},{100,0x39},{110,0x39},{120,0xF6},
+								{130,0x57},{140,0x43},{150,0x42},{160,0x69},{170,0x75},{180,0x16},{190,0x19}};
 
-    //Initializing the array elements with 0 to avoid garbage value
-    short int (*Sensor_noNoise_Vlaue)[2] = {0};
-    int cnt=0;// Number of noise values count
+	/*No Noise Array declaration and initialize to 0*/
+	int Sensor_noNoice_Value[20][2]={0};
 
+	int size = sizeof(Sensor_Values)/sizeof(Sensor_Values[0]);// to find the size of array
+	int NoNoiseCnt=0;
 
-    printf("Sensor Values are:\n");
-    for(int i=0;i<20;i++, printf("\n"))
-            printf("%hd , %x",Sensor_Values[i][0], Sensor_Values[i][1]);
+	puts("20 Sample Data:\n");
+	Disp(Sensor_Values,size);
 
-   //Output 1 : Print the time stamp where data is coming as noise.
+	puts("\n---------------------------------------------------------------------------------------");
 
-    printf("Time stamp where data is coming as noise:\n");
-    for(int i=0;i<20;i++)
-    {
-            if((Sensor_Values[i][1] < 0x05) || (Sensor_Values[i][1] > 0xF0 ))
-            {
-            	printf("%d msec, ", Sensor_Values[i][0]);
-            	cnt++;// Number of noise values count
-            }
+	puts("\nTime stamp where data is coming as noise:\n");
+	for(int i=0;i<size;i++)
+	{
+		if(CheckNoise(Sensor_Values[i][1]))
+		{
+			printf("%d msec , ", Sensor_Values[i][0]);fflush(stdout);
+		}
+		else
+		{
+			Sensor_noNoice_Value[NoNoiseCnt][0] = Sensor_Values[i][0];//Assign the no noise time stamp
+			Sensor_noNoice_Value[NoNoiseCnt][1] = Sensor_Values[i][1];//Assign the no noise data
+			NoNoiseCnt++;//Increment the index of Sensor_noNoice_Value array to add no noise data
+		}
+	}
+	puts("\n---------------------------------------------------------------------------------------");
+	puts("\nNo-Noise Data:");
+	Disp(Sensor_noNoice_Value,NoNoiseCnt);
 
-    }
-    printf("\n");
-
-    //Output 2 : Delete the noise data and write valid sensor values to new 2-dimension array
-   // “Sensor_noNoice_Vlaue[Timestamp_Value][ Sensor_Data]” and print them – timestamp and values.
-
-    //No Noise = Sensor_Values - cnt
-    //No Noise = 20 - cnt
-
-    Sensor_noNoise_Vlaue = calloc(20-cnt,sizeof(Sensor_Values[0])); //Creating the array to store valid sensor values
-    printf("Valid sensor values w.r.to time with NO noise:\n");
-    for(int i=0;i<20;i++)
-        {
-    			if((Sensor_Values[i][1] >= 0x05) && (Sensor_Values[i][1] < 0xF0 ))
-                {
-                	Sensor_noNoise_Vlaue[i][0] = Sensor_Values[i][0];
-                	Sensor_noNoise_Vlaue[i][1] = Sensor_Values[i][1];
-                	printf(" %d msec , %x | ",Sensor_noNoise_Vlaue[i][0], Sensor_noNoise_Vlaue[i][1]);
-                }
-    	}
-
-    return 0;
 }
