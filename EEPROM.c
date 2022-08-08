c151be8f1c022ed8737962fa9b3cf1cbcfa7499d
SIDARDUINO <108972746+SIDARDUINO@users.noreply.github.com>
2022-08-08 09:52:43 +0530

    Added EEPROM.c file Version 0.1

---
+++ EEPROM.c
@@ -0,0 +1,96 @@
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config OSCS = OFF       // Oscillator System Clock Switch Enable bit (Oscillator system clock switch option is disabled (main oscillator is source))
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = OFF        // Brown-out Reset Enable bit (Brown-out Reset disabled)
+#pragma config BORV = 25        // Brown-out Reset Voltage bits (VBOR set to 2.5V)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 128      // Watchdog Timer Postscale Select bits (1:128)
+
+// CONFIG4L
+#pragma config STVR = OFF       // Stack Full/Underflow Reset Enable bit (Stack Full/Underflow will not cause Reset)
+#pragma config LVP = OFF        // Low-Voltage ICSP Enable bit (Low-Voltage ICSP disabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000200-001FFFh) not code protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot Block (000000-0001FFh) not code protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000200-001FFFh) not write protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0001FFh) not write protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000200-001FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from Table Reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from Table Reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from Table Reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+
+#include <xc.h>
+#include <pic18f458.h>
+#define _XTAL_FREQ 2000000
+
+
+unsigned char readEEPROM(unsigned char address)
+{
+EEADR = address; //Address to be read
+EECON1bits.EEPGD = 0; //Selecting EEPROM Data Memory
+EECON1bits.RD = 1; //Initialise read cycle
+while(EECON1bits.RD);
+return EEDATA; //Returning data
+}
+
+void writeEEPROM(unsigned char address, unsigned char data)
+{
+unsigned char INTCON_SAVE; //To save INTCON register value
+EEADR = address; //Address to write
+EEDATA = data; //Data to write
+EECON1bits.EEPGD = 0; //Selecting EEPROM Data Memory
+EECON1bits.WREN = 1; //Enable writing of EEPROM
+INTCON_SAVE=INTCON; //Backup INCON interupt register
+INTCON=0; //Diables the interrupt
+EECON2=0x55; //Required sequence for write to internal EEPROM
+EECON2=0xAA; //Required sequence for write to internal EEPROM
+EECON1bits.WR = 1; //Initialise write cycle
+INTCON = INTCON_SAVE; //Enables Interrupt
+EECON1bits.WREN = 0; //To disable write
+while(PIR2bits.EEIF == 0); //Checking for complition of write operation
+PIR2bits.EEIF = 0; //Clearing EEIF bit
+}
+
+void main(void) 
+{
+    TRISA = 0x00; //PORT C as output port
+    writeEEPROM(0, 0b00001111); //writing data to first memory location and then goes upto last location
+    __delay_ms(20);
+    LATA = readEEPROM(0);
+    while(1);
+}
+
+
+
+
+
